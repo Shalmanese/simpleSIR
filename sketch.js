49a35d49900947803a3b4e9de421fce1aeae28d3
Xianhang Zhang <shalmanese@gmail.com>
2020-09-14 16:42:28 +1000

    refactored.

--- sketch.js
+++ sketch.js
@@ -39,103 +39,110 @@ var infectionChance = 0.20;
 var numAgents = 200;
 var gHistory = [];
 
-function setup() {
-	createCanvas(canvasSize+1000, canvasSize);
-	setupGraph();
-	resetSim();
-}
+var grapharea = function (p) {
 
-function setupGraph(){
-	stroke(0);
-	strokeWeight(2);
-	line (canvasSize + 100, 0, canvasSize + 100, canvasSize);
-	strokeWeight(3);
-	line (canvasSize + 100, canvasSize, 3 * canvasSize + 100, canvasSize);
-	strokeWeight(1);
-	for(var i = 1; i < gHistory.length; i++) {
-		stroke(255, 0, 0);
-		line(canvasSize + 100+i-1, canvasSize-gHistory[i-1][0]*(canvasSize/numAgents), canvasSize + 100+i, canvasSize-gHistory[i][0]*(canvasSize/numAgents));
-		stroke(0, 255, 0);
-		line(canvasSize + 100+i-1, canvasSize-gHistory[i-1][1]*(canvasSize/numAgents), canvasSize + 100+i, canvasSize-gHistory[i][1]*(canvasSize/numAgents));
-		stroke(0, 0, 128);
-		line(canvasSize + 100+i-1, canvasSize-gHistory[i-1][2]*(canvasSize/numAgents), canvasSize + 100+i, canvasSize-gHistory[i][2]*(canvasSize/numAgents));
+	p.setup = function() {
+		p.createCanvas(1000, canvasSize);
+		p.setupGraph();
 	}
-}
 
-function resetSim(){
-	agents = [];
-	gHistory = [];
-	for(var i = 0; i < numAgents; i++) {
-	    agents.push(new Agent(random(canvasSize), random(canvasSize), random(PI)));
-	}
-	agents[0].infect();
-	agents[1].infect();
-	agents[2].infect();
-	agents[3].infect();
-	agents[0].iDays = random(100);
-	agents[1].iDays = random(100);
+	p.draw = function() {
+		p.setupGraph();
+	}	
 	
-}
-function draw() {
-	background(255);
-	setupGraph();
-	agents.forEach(drawAgent);
-	if (status == "start"){
-		infect(agents);
-	}
-  // if (mouseIsPressed) {
-  //   fill(0);
-  // } else {
-  //   fill(255);
-  // }
-  //ellipse(mouseX, mouseY, 80, 80);
-}
-
-function infect(agents) {
-	agents.forEach(function(a){a.move(canvasSize,canvasSize)});
-	var threshold = 20;
-	for(var i = 0; i < agents.length; i++) {
-		if (agents[i].status == "infected" && agents[i].iDays < 70){
-			for(var j = 0; j < agents.length; j++) {
-				if (agents[j].status == "uninfected" && idist(agents[i],agents[j]) < threshold)
-					agents[j].infect();
-			}			
+	p.setupGraph = function() {
+		p.stroke(0);
+		p.strokeWeight(2);
+		p.line (0, 0, 0, canvasSize);
+		p.strokeWeight(3);
+		p.line (0, canvasSize, 1200, canvasSize);
+		p.strokeWeight(1);
+		for(var i = 1; i < gHistory.length; i++) {
+			p.stroke(255, 0, 0);
+			p.line(0+i-1, canvasSize-gHistory[i-1][0]*(canvasSize/numAgents), 0+i, canvasSize-gHistory[i][0]*(canvasSize/numAgents));
+			p.stroke(0, 255, 0);
+			p.line(0+i-1, canvasSize-gHistory[i-1][1]*(canvasSize/numAgents), 0+i, canvasSize-gHistory[i][1]*(canvasSize/numAgents));
+			p.stroke(0, 0, 128);
+			p.line(0+i-1, canvasSize-gHistory[i-1][2]*(canvasSize/numAgents), 0+i, canvasSize-gHistory[i][2]*(canvasSize/numAgents));
 		}
-		agents[i].tick();
 	}
-	var nInf = 0;
-	var nUinf = 0;
-	var nImm = 0;
-	for(var i = 0; i < agents.length; i++) {
-		if (agents[i].status == "infected")
-			nInf = nInf + 1;
-		if (agents[i].status == "uninfected")
-			nUinf = nUinf + 1;
-		if (agents[i].status == "immune")
-			nImm = nImm + 1;
+}
+var playarea = function (p) {	
+	p.setup = function() {
+		p.createCanvas(canvasSize, canvasSize);
+		p.resetSim();
 	}
-	gHistory.push([nInf, nUinf, nImm]);
+
+	p.resetSim = function(){
+		agents = [];
+		gHistory = [];
+		for(var i = 0; i < numAgents; i++) {
+			agents.push(new Agent(p.random(canvasSize), p.random(canvasSize), p.random(p.PI)));
+		}
+		agents[0].infect();
+		agents[1].infect();
+		agents[2].infect();
+		agents[3].infect();
+		agents[0].iDays = p.random(100);
+		agents[1].iDays = p.random(100);
+	
+	}
+	p.draw = function() {
+		p.background(255);
+		//setupGraph();
+		agents.forEach(drawAgent);
+		if (status == "start"){
+			p.infect(agents);
+		}
+	}
+
+	p.infect = function(agents) {
+		agents.forEach(function(a){a.move(canvasSize,canvasSize)});
+		var threshold = 20;
+		for(var i = 0; i < agents.length; i++) {
+			if (agents[i].status == "infected" && agents[i].iDays < 70){
+				for(var j = 0; j < agents.length; j++) {
+					if (agents[j].status == "uninfected" && idist(agents[i],agents[j]) < threshold)
+						agents[j].infect();
+				}			
+			}
+			agents[i].tick();
+		}
+		var nInf = 0;
+		var nUinf = 0;
+		var nImm = 0;
+		for(var i = 0; i < agents.length; i++) {
+			if (agents[i].status == "infected")
+				nInf = nInf + 1;
+			if (agents[i].status == "uninfected")
+				nUinf = nUinf + 1;
+			if (agents[i].status == "immune")
+				nImm = nImm + 1;
+		}
+		gHistory.push([nInf, nUinf, nImm]);
+	}
+
+	function idist(i, j){
+		var distance = Math.abs(i.x - j.x) + Math.abs(i.y - j.y);
+		return distance;
+	}
+	function drawAgent(a){
+		var dotSize = 4;
+		var arrowSize = 6;
+		p.strokeWeight(1);
+		if (a.status == "uninfected")
+			p.stroke(0, 255, 0);
+		else if (a.status == "infected")
+			p.stroke(255, 0, 0);
+		else
+			p.stroke(0, 0, 128)
+			p.ellipse(a.x, a.y, dotSize, dotSize);
+		p.line(a.x, a.y, a.x+a.moveX*arrowSize, a.y+a.moveY*arrowSize);
+	}
 }
 
-function idist(i, j){
-	var distance = Math.abs(i.x - j.x) + Math.abs(i.y - j.y);
-	if (distance < 100)
-		distance = distance;
-	return distance;
-}
-function drawAgent(a){
-	var dotSize = 4;
-	var arrowSize = 6;
-	strokeWeight(1);
-	if (a.status == "uninfected")
-		stroke(0, 255, 0);
-	else if (a.status == "infected")
-		stroke(255, 0, 0);
-	else
-		stroke(0, 0, 128)
-	ellipse(a.x, a.y, dotSize, dotSize);
-	line(a.x, a.y, a.x+a.moveX*arrowSize, a.y+a.moveY*arrowSize);
-}
+var p5play = new p5(playarea, 'playarea');
+var p5graph = new p5(grapharea, 'grapharea');
 
 function startSketch(){
 	status = "start";
@@ -146,12 +153,12 @@ function stopSketch(){
 }
 
 function resetSketch(){
-	resetSim();
+	p5play.resetSim();
 }
 
 function runSketch(){
-	resetSim();
+	p5play.resetSim();
 	for(var i = 0; i < 1200; i++) {
-		infect(agents);
+		p5play.infect(agents);
 	}	
 }
